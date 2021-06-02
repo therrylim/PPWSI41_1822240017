87883956e6fdee70f21d8ea7f02a7e9e26c0556c
Therry <therry@mhs.mdp.ac.id>
2021-06-02 23:52:46 +0700

    Belajar Migration, DB Facade, Query Builder dan Eloquent

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,66 +3,111 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\mahasiswa;
 
 class MahasiswaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function insert()
     {
-        return array("pesan" => "List Data Mahasiswa",
-        "data" => array()
+        $result = DB::insert('insert into mahasiswas (npm, nama_mahasiswa, tempat_lahir, tanggal_lahir, alamat, created_at)
+        values (?, ?, ?, ?, ?, ?)', ['1822240090', 'Gabriel', 'Medan', '2000-08-06', 'Jl. Merdeka ', now()]);
+        dump($result);
+    }
+
+    public function update()
+    {
+        $result = DB::update('update mahasiswas set nama_mahasiswa = "Stella",
+        updated_at = now() where npm = ?', ['1822240099']);
+        dump($result);
+    }
+
+    public function delete()
+    {
+        $result = DB::delete('delete from mahasiswas where npm = ?', ['1822240090']);
+        dump($result);
+    }
+
+    public function select()
+    {
+        $kampus = "Universitas Multi Data Palembang";
+        $result = DB::select('select * from mahasiswas');
+        //dump($result);
+        return view('mahasiswa.index', ['allmahasiswa' => $result, 'kampus' => $kampus]);
+    }
+
+    public function insertQb()
+    {
+        $result = DB::table('mahasiswas')->insert(
+            [
+                'npm' => '1822240017',
+                'nama_mahasiswa' => 'Therry',
+                'tempat_lahir' => 'Palembang',
+                'tanggal_lahir' => '2000-03-06',
+                'alamat' => 'Jl Bukit Besar',
+                'created_at' => now()
+            ]
         );
+        dump($result);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function updateQb()
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        return array(
-            "pesan" => "Detail Mahasiswa dengan id : $id",
-            "data" => array()
+        $result = DB::table('mahasiswas')->where('npm', '1822240017')->update(  
+            [
+                'nama_mahasiswa' => 'Therry',
+                'updated_at' => now()
+            ]
         );
+        dump($result);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function deleteQb()
     {
-        //
+        $result = DB::table('mahasiswas')->where('npm', '=', '1822240017')->delete();
+        dump($result);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function selectQb()
     {
-        //
+        $kampus = "Universitas Multi Data Palembang";
+        $result = DB::table('mahasiswas')->get();
+        // dump($result);
+        return view('mahasiswa.index', ['allmahasiswa' => $result, 'kampus' => $kampus]);
+    }
+
+    public function insertElq()
+    {
+        $mahasiswa = new Mahasiswa; // instansiasi class Mahasiswa
+        $mahasiswa->npm = '1822240089'; // isi properti
+        $mahasiswa->nama_mahasiswa = 'Tasya';
+        $mahasiswa->tempat_lahir = 'Palembang';
+        $mahasiswa->tanggal_lahir = '2001-09-20';
+        $mahasiswa->alamat = 'Jl Sumpah Pemuda';
+        $mahasiswa->save(); // menyimpan data ke tabel mahasiswa
+        dump($mahasiswa); // lihat isi $mahasiswa
+    }
+
+    public function updateElq()
+    {
+        $mahasiswa = Mahasiswa::where('npm', '1822240089')->first(); // cari data berdasarkan npm
+        $mahasiswa->nama_mahasiswa = 'Vinny';
+        $mahasiswa->save();
+        dump($mahasiswa);
+    }
+
+    public function deleteElq()
+    {
+        $mahasiswa = Mahasiswa::where('npm', '1822240089')->first(); // caridata
+        $mahasiswa->delete(); // hapus data npm 1822240002
+        dump($mahasiswa);
+    }
+
+    public function selectElq()
+    {
+        $kampus = "Universitas Multi Data Palembang";
+        $result = Mahasiswa::all();
+        // dump($result);
+        return view('mahasiswa.index', ['allmahasiswa' => $result, 'kampus' => $kampus]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MahasiswaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/mahasiswa/insert', [MahasiswaController::class, 'insert']);
+Route::get('/mahasiswa/update', [MahasiswaController::class, 'update']);
+Route::get('/mahasiswa/delete', [MahasiswaController::class, 'delete']);
+Route::get('/mahasiswa/select', [MahasiswaController::class, 'select']);
+Route::get('/mahasiswa/insert-qb', [MahasiswaController::class, 'insertQb']);
+Route::get('/mahasiswa/update-qb', [MahasiswaController::class, 'updateQb']);
+Route::get('/mahasiswa/delete-qb', [MahasiswaController::class, 'deleteQb']);
+Route::get('/mahasiswa/select-qb', [MahasiswaController::class, 'selectQb']);
+Route::get('/mahasiswa/insert-elq', [MahasiswaController::class, 'insertElq']);
+Route::get('/mahasiswa/update-elq', [MahasiswaController::class, 'updateElq']);
+Route::get('/mahasiswa/delete-elq', [MahasiswaController::class, 'deleteElq']);
+Route::get('/mahasiswa/select-elq', [MahasiswaController::class, 'selectElq']);
